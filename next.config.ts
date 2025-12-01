80a6750a534c79859d696e2b15feb457b9968f9c
Akash <dwivediakash1702@gmail>
2025-12-01 16:07:23 +0530

    Completed setup and installed shadcn

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental : {
+    turbopackFileSystemCacheForDev : true,
+  }
+  /* config options here */
+};
+
+export default nextConfig;
